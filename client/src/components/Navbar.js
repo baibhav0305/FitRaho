858aa451d51597812e104ed58dc9e725dd159e58
Baibhav <baibhav030501@gmail.com>
2022-06-21 23:22:41 +0530

    navbar logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             <h2>FitRaho</h2>
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
