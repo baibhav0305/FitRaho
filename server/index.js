d920c75e8ba054c0f2d201eee985a0bf9acdd7d5
Baibhav <baibhav030501@gmail.com>
2022-06-22 00:30:22 +0530

    index --routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const { spawn } = require("child_process");
 const authRoute = require("./routes/auth");
+const exerciseRoute = require("./routes/exercise");
+const userRoute = require("./routes/user");
 
 const client = require("twilio")(
   "AC4b637e7ed06a85954387e5282659a26b",
@@ -15,8 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 // +1 334 459 9701
 // +16165778934;
-
-const User = require("./model/UserSchema");
 
 app.use(express.json());
 app.use(cors());
@@ -40,150 +36,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoute);
-
-app.get("/api/biceps", (req, res) => {
-  let dataToSend;
-  const python = spawn("python", ["biceps.py"]);
-  python.stdout.on("data", (data) => {
-    console.log("Biceps script started...");
-    dataToSend = data.toString();
-  });
-  python.on("close", () => {
-    console.log(`Biceps script ended...`);
-    res.json({ count: dataToSend });
-  });
-});
-
-app.get("/api/triceps", (req, res) => {
-  let dataToSend;
-  const python = spawn("python", ["biceps.py"]);
-  python.stdout.on("data", (data) => {
-    console.log("Triceps script started...");
-    dataToSend = data.toString();
-  });
-  python.on("close", () => {
-    console.log(`Triceps script ended...`);
-    res.json({ count: dataToSend });
-  });
-});
-
-app.get("/api/squats", (req, res) => {
-  let dataToSend;
-  const python = spawn("python", ["check1.py"]);
-  python.stdout.on("data", (data) => {
-    console.log("Squats script started...");
-    dataToSend = data.toString();
-  });
-  python.on("close", () => {
-    console.log(`Squats script ended...`);
-    res.json({ count: dataToSend });
-  });
-});
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-app.post("/api/post", async (req, res) => {
-  const token = req.headers["x-access-token"];
-  let { count, exercise } = req.body;
-  console.log(count, exercise);
-  try {
-    const decoded = jwt.verify(token, "secret123");
-    const email = decoded.email;
-
-    const date = new Date().getDate();
-    const month = new Date().getMonth();
-    const year = new Date().getFullYear();
-    const d = new Date(year, month, date) + 5.5 * 60 * 60 * 1000;
-    console.log(d + 5.5 * 60 * 60 * 1000);
-
-    const user = await User.findOne({ email: email }, { data: { $slice: -1 } });
-
-    const ndata = user.data;
-    console.log(ndata);
-    // console.log(ndata[0].date);
-    // console.log(ndata[0].biceps);
-    // console.log(ndata[0].triceps);
-    // console.log(ndata[0].calories);
-    // console.log(user);
-    if (ndata[0].date === d) {
-      if (exercise === "biceps") {
-        let ctr = count + ndata[0].calories;
-        count = count + ndata[0].biceps;
-        // console.log(ctr);
-        await User.findOneAndUpdate(
-          { email: email, "data.date": d },
-          { $set: { "data.$.biceps": count, "data.$.calories": ctr } }
-        );
-      } else if (exercise === "triceps") {
-        let ctr = count + ndata[0].calories;
-        count = count + ndata[0].triceps;
-        await User.findOneAndUpdate(
-          { email: email, "data.date": d },
-          { $set: { "data.$.triceps": count, "data.$.calories": ctr } }
-        );
-      } else {
-        let ctr = 2 * count + ndata[0].squats;
-        count = count + ndata[0].squats;
-        await User.findOneAndUpdate(
-          { email: email, "data.date": d },
-          { $set: { "data.$.squats": count, "data.$.calories": ctr } }
-        );
-      }
-    } else {
-      let newData;
-      if (exercise === "biceps") {
-        newData = {
-          date: d,
-          biceps: count,
-          triceps: 0,
-          squats: 0,
-          calories: 0,
-        };
-      } else if (exercise === "triceps") {
-        newData = {
-          date: d,
-          biceps: 0,
-          triceps: count,
-          squats: 0,
-          calories: 0,
-        };
-      } else {
-        newData = {
-          date: d,
-          biceps: 0,
-          triceps: 0,
-          squats: count,
-          calories: 0,
-        };
-      }
-      await User.findOneAndUpdate(
-        { email: email },
-        { $push: { data: newData } }
-      );
-    }
-    // console.log(user);
-    // console.log(data);
-
-    res.json({ status: "ok" });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "error", error: "Invalid Token" });
-  }
-});
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-app.get("/api/history", async (req, res) => {
-  const token = req.headers["x-access-token"];
-
-  try {
-    const decoded = jwt.verify(token, "secret123");
-    const email = decoded.email;
-    const user = await User.findOne({ email: email });
-
-    return res.json({ status: "ok", data: user.data });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "error", error: "invalid token" });
-  }
-});
+app.use("/api/exercise", exerciseRoute);
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
